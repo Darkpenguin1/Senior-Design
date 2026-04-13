3ec0eb4c80618b80ce7dfc85f11d22abdca3ea4c
James McKenna <jmcken19@charlotte.edu>
2026-04-13 18:09:33 -0400

    Added changes to end of code and updated URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", //Springboot API
+  baseURL: "http://127.0.0.1:8080", //Springboot API
   headers: { "Content-Type": "application/json" },
 });
 
@@ -12,28 +12,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-
-//After that call the api via fhe frontend somethin like that it all depends on the frontend 
-//code components though and springboot endpoints 
-
-const response = await api.post('/register', {
-        email: formData.email,
-        username: formData.username,
-        password: formData.password,
-      });
-
-      if (response.status === 200 || response.status === 201) {
-        setSuccess("Account Successfully Created!");
-        setError('');
-
-        const tokenResponse = await api.post('/token', new URLSearchParams({
-          username: formData.email,
-          password: formData.password
-        }));
-
-        if (tokenResponse.data.token){
-          localStorage.setItem('token', tokenResponse.data.token);
-
-          window.location.href = '/';
-        }
-}
