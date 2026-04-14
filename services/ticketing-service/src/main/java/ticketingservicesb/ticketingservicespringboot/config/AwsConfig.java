b846d3440be4a047369c054e80ac792937436dbb
fperaza <Frank.E.Peraza@outlook.com>
2026-04-14 19:13:19 -0400

    Added image processing and upload to s3

--- services/ticketing-service/src/main/java/ticketingservicesb/ticketingservicespringboot/config/AwsConfig.java
+++ services/ticketing-service/src/main/java/ticketingservicesb/ticketingservicespringboot/config/AwsConfig.java
@@ -2,6 +2,7 @@ package ticketingservicesb.ticketingservicespringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
@@ -10,5 +11,10 @@ public class AwsConfig {
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.create();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.create();
     }
 }
